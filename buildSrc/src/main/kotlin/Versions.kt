1eef943d461b1e29ec5d08366075bf31da27c0a6
neworld <aneworld@gmail.com>
2018-09-23 23:15:58 +0300

    Add more tests

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     val kupiter = "1.0.0"
-    val junit5Version = "5.2.0"
+    val junit5Version = "5.3.1"
     val kotlinArgParser = "2.0.7"
     val coroutines = "0.26.1-eap13"
 }
--- src/main/kotlin/lt/neworld/gradle/logchopper/Processor.kt
+++ src/main/kotlin/lt/neworld/gradle/logchopper/Processor.kt
@@ -14,10 +14,12 @@ class Processor(private val input: File, private val output: File) {
         val inputStream = input.inputStream()
         val splitter = Splitter()
         runBlocking {
+            var index = 0
             for (chunk in splitter.split(inputStream)) {
-                File(output, "${chunk.name}.txt").outputStream().use {
+                File(output, "${index.toString().padStart(4, '0')}-${chunk.name}.txt").outputStream().use {
                     chunk.input.copyTo(it)
                 }
+                index++
             }
         }
     }
